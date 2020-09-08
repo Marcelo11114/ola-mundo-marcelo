dd120d743b50dcfeeef47a7e362f6769eda755a3
Marcelo11114 <dedosearte@hotmail.com>
2020-09-07 23:03:31 -0300

    olamundo nome marcelo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ola_mundo_marcelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola!!! mundo");
+            Console.WriteLine("meu nome é Marcelo");
+        }
+    }
+}
